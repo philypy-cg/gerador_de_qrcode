e7ab89cd4239d78a1621849c32144df530600b9a
philypy-cg <phcg.dev@gmail.com>
2023-02-09 00:15:38 -0300

    script inicial

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+const inputValue = document.querySelector("#inputValue");
+const btnValue = document.querySelector("#btnValue");
+const imgQrCode = document.querySelector("#imgQrCode");
+const wrapper = document.querySelector(".wrapper");
+let valueDefault;
