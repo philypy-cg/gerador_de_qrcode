6848909ec05b6c9104ae0dbb5bc82b534a94074e
philypy-cg <phcg.dev@gmail.com>
2023-02-10 00:02:43 -0300

    modificando js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,7 @@ const btnValue = document.querySelector("#btnValue");
 const imgQrCode = document.querySelector("#imgQrCode");
 const wrapper = document.querySelector(".wrapper");
 let valueDefault;
+
+// https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=Example
+
+btnValue.addEventListener;
